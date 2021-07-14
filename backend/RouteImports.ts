1fba1a2d7c6c56526d738ca02591008a11ec22ce
Joel Denning <joeldenning@gmail.com>
2021-07-14 10:09:20 -0600

    Batch create fields endpoint (#17)
    * Batch create fields endpoint
    
    * Rhiannon's feedback

--- backend/RouteImports.ts
+++ backend/RouteImports.ts
@@ -3,5 +3,7 @@ import "./Nouns/PostNouns.js";
 import "./Nouns/PatchNouns.js";
 import "./Nouns/DeleteNouns.js";
 
+import "./Fields/BatchPostFields.js";
+
 // WebApp should be at the bottom
 import "./WebApp/WebApp.js";
