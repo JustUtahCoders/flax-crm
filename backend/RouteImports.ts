2cf939cda0e5bb4d1476c102fbe201cb6b48ce46
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-07-14 21:16:17 -0700

    Server side api auth complete.  Implement server side HTTP 401 whenever non-auth apis are called without being authenticated. Resolves #6 (#20)

--- backend/RouteImports.ts
+++ backend/RouteImports.ts
@@ -5,8 +5,7 @@ import "./Nouns/GetNouns.js";
 import "./Nouns/PostNouns.js";
 import "./Nouns/PatchNouns.js";
 import "./Nouns/DeleteNouns.js";
-
 import "./Fields/BatchPostFields.js";
 
-// WebApp should be at the bottom
+// WebApp should be at the bottom, as it's the default route
 import "./WebApp/WebApp.js";
