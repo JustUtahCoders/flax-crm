819aa5793df245911c422efdfa017ddbc28f7e4b
Joel Denning <joeldenning@gmail.com>
2021-10-29 17:10:18 -0600

    More tests

--- frontend/Styleguide/Icon.test.tsx
+++ frontend/Styleguide/Icon.test.tsx
@@ -5,13 +5,27 @@ import { Icon, IconVariant } from "./Icon";
 describe("<Icon />", () => {
   it(`always renders a <title>`, () => {
     Object.keys(IconVariant).forEach((iconVariant) => {
-      const w = render(<Icon variant={IconVariant.close} />);
+      const w = render(<Icon variant={iconVariant} />);
 
       expect(w.baseElement.querySelector("title")).toBeInTheDocument();
 
-      w.rerender(<Icon variant={IconVariant.close} alt="Donkey Kong" />);
+      w.rerender(<Icon variant={iconVariant} alt="Donkey Kong" />);
 
       expect(w.queryByTitle("Donkey Kong")).toBeInTheDocument();
+    });
+  });
+
+  it(`respects the size prop`, () => {
+    Object.keys(IconVariant).forEach((iconVariant) => {
+      const w = render(<Icon variant={iconVariant} alt="the image" />);
+      let svgEl = w.getByTitle("the image").parentElement;
+      expect(svgEl.getAttribute("height")).toBe(`16px`);
+      expect(svgEl.getAttribute("width")).toBe(`16px`);
+
+      w.rerender(<Icon variant={iconVariant} alt="the image" size={26} />);
+      svgEl = w.getByTitle("the image").parentElement;
+      expect(svgEl.getAttribute("height")).toBe(`26px`);
+      expect(svgEl.getAttribute("width")).toBe(`26px`);
     });
   });
 });
