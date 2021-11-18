fa44a026e4764b90e6ee8859348cc183b803ef15
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-11-18 09:31:02 -0800

    Resolve comment.  Define JWT association in model

--- backend/DB/models/jwt.cjs
+++ backend/DB/models/jwt.cjs
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.JWT.belongsTo(models.User);
     }
   }
   JWT.init(
