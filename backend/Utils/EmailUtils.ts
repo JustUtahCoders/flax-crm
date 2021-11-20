6233b2f7106ce4fa7761a458835bcd25238ba50d
Rhiannon Stanford <mochi@Rhiannons-iMac.local>
2021-11-19 16:53:03 -0800

    Add html content to email with styling

--- backend/Utils/EmailUtils.ts
+++ backend/Utils/EmailUtils.ts
@@ -4,7 +4,25 @@ import { encode } from "js-base64";
 import dotenv from "dotenv";
 
 async function makeEmail({ to, from, subject }) {
-  const message = `This is a test email for reset password.`;
+  const message = `
+  <div style="width: 60vw; margin: 4rem auto auto auto; color: #403F3D;
+  ">
+  <h1 style="color:#2a467b">Hello!</h1>
+  <p style="margin-bottom:5rem">You are receiving this email because we received a password reset request for your account.</p>
+  <div style="display:flex; width:100vw; align-items:center; justify-content:center;">
+  <a href="https://www.google.com" style="color:white; background-color: #2a467b; text-decoration: none; padding:1rem; border-radius:5px; width:20vw; text-align:center; ">Reset Password</a>
+  </div>
+  </br>
+  <p style="margin-top:5rem">If you are having trouble clicking the <span style="font-style:italic">"Reset Password"</span> button, copy and paste the following URL into your web browser</.p>
+  </br>
+  https://www.google.com
+  </div>
+  <footer style="margin-top:4rem">
+  <hr></hr>
+  <p style="margin:1rem; text-align:center; color: #403F3D;">Just Utah Coders</p>
+  </footer>
+  `;
+  // put html here, use anchor tag, the JWT goes in as a query string
 
   // the white space is important, template literals are space sensitive
   const str = `
