04b089a66cec77572418f39ff5ee9421b577e0df
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-11-23 13:49:07 -0800

    Save password reset jwt to db (#59)
    * Add html content to email with styling
    
    * Install jsonwebtoken
     dependency
    
    * Add JWT and html content to email
    
    * Remove id from user model because it is in the default model.  This will be removed in a refactor
    
    * Add jsonwebtoken dependency
    
    * Add keys directory to gitignore
    
    * Ajust foreign key in jwt model to resolve bug
    
    * WIP in EmailUtils before refactor of logic to BE ResetPassword
    
    * Comments resolved.  Small refactor to move jwt creation to JWTUtils file. Move resetPasswordEmail content to BE ResetPassword
    
    * Move payload and token creation to within conditional that first checks for user in ResetPassword
    
    * Remove console.logs
    
    * Resolve comments
    
    * Remove unused code and import from JWTUtils.ts.  Make secret generic for reusability
    
    Co-authored-by: Rhiannon Stanford <mochi@Rhiannons-iMac.local>

--- backend/Utils/JWTUtils.ts
+++ backend/Utils/JWTUtils.ts
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 const { sign, verify } = jwt;
 
 export function makeJWT(payload: object): string {
-  const secret = process.env.JWT_PASSWORD_RESET_SECRET || "secret";
+  const secret = process.env.JWT_SECRET || "secret";
 
   const token = sign(payload, secret, { expiresIn: "1h" });
+
   return token;
 }
