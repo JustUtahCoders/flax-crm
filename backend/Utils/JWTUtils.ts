91f99537e6208fae140e8cd794eb9cfb87fc643e
Rhiannon Stanford <rhiannonstanford@gmail.com>
2021-11-23 13:46:05 -0800

    Remove unused code and import from JWTUtils.ts.  Make secret generic for reusability

--- backend/Utils/JWTUtils.ts
+++ backend/Utils/JWTUtils.ts
@@ -1,14 +1,11 @@
 import jwt from "jsonwebtoken";
-import { JWTModel } from "../DB/models/JWT";
 
 const { sign, verify } = jwt;
 
 export function makeJWT(payload: object): string {
-  const secret = process.env.JWT_PASSWORD_RESET_SECRET || "secret";
+  const secret = process.env.JWT_SECRET || "secret";
 
   const token = sign(payload, secret, { expiresIn: "1h" });
-
-  const userId = payload["userId"];
 
   return token;
 }
